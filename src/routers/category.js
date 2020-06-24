adfb686f7397ff74be5c2257191adfa96eab3da9
elaria <elarya.latif@gmail.com>
2020-06-24 15:35:56 +0200

    array of object

--- src/routers/category.js
+++ src/routers/category.js
@@ -30,5 +30,49 @@ router.post('/categories', upload.single('categoryImg'), async(req, res) => {
         res.status(400).send(error)
     }
 });
-// 
+//============================
+// List All Categories
+router.get('/categories', async(req, res) => {
+    Category.find(function(err, category) {
+        if (!err) {
+            return res.send({ message: 'Categories List', category });
+        } else {
+            res.status(400).send(error);
+        }
+    });
+});
+//============================
+// Retrieve Single Category
+router.get('/categories/:id', async(req, res) => {
+    Category.findById(req.params.id, function(err, category) {
+        if (!err) {
+            return res.send({ message: 'Category is existed', category });
+        } else {
+            res.status(400).send(error);
+        }
+    });
+});
+//============================
+// Update Single Category
+router.put('/categories/:id', upload.single('categoryImg'), async(req, res) => {
+    Category.findById(req.params.id, function(err, category) {
+        category.name = req.body.name;
+        category.details = req.body.details;
+        category.rate = req.body.rate;
+        category.categoryImg = req.file.path;
+        category.products = req.body.products;
+        // Save Updated
+
+        return category.save(function(err) {
+            if (!err) {
+                return res.send({ message: 'Category is updated', category });
+            } else {
+                res.status(400).send(error);
+            }
+            return res.send(category);
+        });
+
+    });
+});
+//============================
 module.exports = router
